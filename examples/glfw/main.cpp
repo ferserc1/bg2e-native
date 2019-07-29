fb3a02bc93860ee40c65029123eb845270c96296
ferserc1 <ferserc1@gmail.com>
2019-07-29 15:19:05 +0200

    Vulkan example app: update command buffers each frame

--- examples/glfw/main.cpp
+++ examples/glfw/main.cpp
@@ -23,7 +23,7 @@ class MyDelegate : public bg2wnd::WindowDelegate {
 public:
     MyDelegate(bool enableValidation) :_enableValidationLayers(enableValidation) {}
 
-	int _maxFramesInFlight = 2;
+	int _maxFramesInFlight;
 
 	VkShaderModule _vertShaderModule;
 	VkShaderModule _fragShaderModule;
@@ -313,7 +313,8 @@ public:
 		VkCommandPoolCreateInfo poolInfo = {};
 		poolInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
 		poolInfo.queueFamilyIndex = _instance->renderPhysicalDevice()->queueIndices().graphicsFamily;
-		poolInfo.flags = 0;
+		poolInfo.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
+		//poolInfo.flags = 0;
 
 		if (vkCreateCommandPool(_instance->renderDevice()->device(), &poolInfo, nullptr, &_commandPool) != VK_SUCCESS) {
 			throw std::runtime_error("Error creating command pool");
@@ -329,9 +330,41 @@ public:
 		if (vkAllocateCommandBuffers(_instance->renderDevice()->device(), &allocInfo, _commandBuffers.data()) != VK_SUCCESS) {
 			throw std::runtime_error("Error allocating command buffers");
 		}
-		
+
+		////// CPU-GPU rendering sinchronization
+		_maxFramesInFlight = _swapChain->images().size();
+		_imageAvailableSemaphore.resize(_maxFramesInFlight);
+		_renderFinishedSemaphore.resize(_maxFramesInFlight);
+		_inFlightFences.resize(_maxFramesInFlight);
+
+		VkSemaphoreCreateInfo semaphoreInfo = {};
+		semaphoreInfo.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
+
+		VkFenceCreateInfo fenceInfo = {};
+		fenceInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
+		fenceInfo.flags = VK_FENCE_CREATE_SIGNALED_BIT;
+
+		for (size_t i = 0; i < _maxFramesInFlight; ++i) {
+			if (vkCreateSemaphore(_instance->renderDevice()->device(), &semaphoreInfo, nullptr, &_imageAvailableSemaphore[i]) != VK_SUCCESS ||
+				vkCreateSemaphore(_instance->renderDevice()->device(), &semaphoreInfo, nullptr, &_renderFinishedSemaphore[i]) != VK_SUCCESS ||
+				vkCreateFence(_instance->renderDevice()->device(), &fenceInfo, nullptr, &_inFlightFences[i]) != VK_SUCCESS) {
+				throw std::runtime_error("Failed to create sinchronization objects");
+			}
+		}
+    }
+
+    void resize(const bg2math::int2 & size) {
+        std::cout << "resize: " << size.x() << ", " << size.y() << std::endl;
+    }
+
+    void update(float delta) {
+		vkWaitForFences(_instance->renderDevice()->device(), 1, &_inFlightFences[_currentFrame], VK_TRUE, std::numeric_limits<uint64_t>::max());
+		vkResetFences(_instance->renderDevice()->device(), 1, &_inFlightFences[_currentFrame]);
+
 		// Start recording commands
-		for (size_t i = 0; i < _commandBuffers.size(); ++i) {
+		size_t i = _currentFrame;
+//		for (size_t i = 0; i < _commandBuffers.size(); ++i) {
+		vkResetCommandBuffer(_commandBuffers[i], 0);
 			VkCommandBufferBeginInfo beginInfo = {};
 			beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
 			beginInfo.flags = 0;
@@ -363,40 +396,11 @@ public:
 			if (vkEndCommandBuffer(_commandBuffers[i]) != VK_SUCCESS) {
 				throw std::runtime_error("Failed to record command buffer");
 			}
-		}
-
-		////// CPU-GPU rendering sinchronization
-		_imageAvailableSemaphore.resize(_maxFramesInFlight);
-		_renderFinishedSemaphore.resize(_maxFramesInFlight);
-		_inFlightFences.resize(_maxFramesInFlight);
-
-		VkSemaphoreCreateInfo semaphoreInfo = {};
-		semaphoreInfo.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
-
-		VkFenceCreateInfo fenceInfo = {};
-		fenceInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
-		fenceInfo.flags = VK_FENCE_CREATE_SIGNALED_BIT;
-
-		for (size_t i = 0; i < _maxFramesInFlight; ++i) {
-			if (vkCreateSemaphore(_instance->renderDevice()->device(), &semaphoreInfo, nullptr, &_imageAvailableSemaphore[i]) != VK_SUCCESS ||
-				vkCreateSemaphore(_instance->renderDevice()->device(), &semaphoreInfo, nullptr, &_renderFinishedSemaphore[i]) != VK_SUCCESS ||
-				vkCreateFence(_instance->renderDevice()->device(), &fenceInfo, nullptr, &_inFlightFences[i]) != VK_SUCCESS) {
-				throw std::runtime_error("Failed to create sinchronization objects");
-			}
-		}
-    }
-
-    void resize(const bg2math::int2 & size) {
-        std::cout << "resize: " << size.x() << ", " << size.y() << std::endl;
-    }
-
-    void update(float delta) {
-       // std::cout << "udpate: " << delta << std::endl;
+//		}
     }
 
     void draw() {
-		vkWaitForFences(_instance->renderDevice()->device(), 1, &_inFlightFences[_currentFrame], VK_TRUE, std::numeric_limits<uint64_t>::max());
-		vkResetFences(_instance->renderDevice()->device(), 1, &_inFlightFences[_currentFrame]);
+		
 
 		uint32_t imageIndex;
 		vkAcquireNextImageKHR(
