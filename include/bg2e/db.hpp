4da12fba82e5efbc7b6e4e50463cafe1f8b12f16
Fernando Serrano Carpena <ferserc1@gmail.com>
2019-10-29 16:31:13 +0100

    Xcode project updated

--- include/bg2e/db.hpp
+++ include/bg2e/db.hpp
@@ -5,11 +5,12 @@
 #include <bg2e/db/bg2_reader.hpp>
 #include <bg2e/db/bg2_writter.hpp>
 #include <bg2e/db/binary_parser.hpp>
-#include <bg2e/db/convert.hpp>
-#include <bg2e/db/escaper.hpp>
-#include <bg2e/db/grammar.hpp>
-#include <bg2e/db/parser.hpp>
-#include <bg2e/db/types.hpp>
-#include <bg2e/db/value.hpp>
+#include <bg2e/db/json/convert.hpp>
+#include <bg2e/db/json/escaper.hpp>
+#include <bg2e/db/json/grammar.hpp>
+#include <bg2e/db/json/parser.hpp>
+#include <bg2e/db/json/value.hpp>
+#include <bg2e/db/image_load.hpp>
+#include <bg2e/db/image_write.hpp>
 
 #endif
