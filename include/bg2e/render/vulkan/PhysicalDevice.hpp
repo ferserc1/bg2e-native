ad21da07ffcea1a2f4c7937d1da90237f062763a
Fernando Serrano Carpena <ferserc1@gmail.com>
2024-11-09 13:20:11 +0100

    PhysicalDevice class added

---
+++ include/bg2e/render/vulkan/PhysicalDevice.hpp
@@ -0,0 +1,19 @@
+
+#pragma once
+
+namespace bg2e {
+namespace render {
+namespace vulkan {
+
+class Instance;
+
+class PhysicalDevice {
+public:
+
+    void create(Instance * instance);
+    
+};
+
+}
+}
+}
---
+++ src/bg2e/render/vulkan/PhysicalDevice.cpp
@@ -0,0 +1,14 @@
+
+#include <bg2e/render/vulkan/PhysicalDevice.hpp>
+#include <bg2e/render/vulkan/Instance.hpp>
+
+namespace bg2e {
+namespace render {
+namespace vulkan {
+
+void PhysicalDevice::create(Instance * instance) {
+}
+
+}
+}
+}
