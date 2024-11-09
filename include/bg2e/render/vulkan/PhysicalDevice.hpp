a7be7d5a6b42d46e441b809ba025396abfdda6ca
Fernando Serrano Carpena <ferserc1@gmail.com>
2024-11-09 15:32:23 +0100

    PhysicalDevice utility functions added

--- include/bg2e/render/vulkan/PhysicalDevice.hpp
+++ include/bg2e/render/vulkan/PhysicalDevice.hpp
@@ -1,6 +1,9 @@
 
 #pragma once
 
+#include <bg2e/render/vulkan/common.hpp>
+#include <optional>
+
 namespace bg2e {
 namespace render {
 namespace vulkan {
@@ -9,9 +12,24 @@ class Instance;
 
 class PhysicalDevice {
 public:
+    struct QueueFamilyIndices
+    {
+        std::optional<uint32_t> graphics;
+        std::optional<uint32_t> present;
+        
+        inline bool isComplete()
+        {
+            return graphics.has_value() && present.has_value();
+        }
+        
+        static QueueFamilyIndices get(VkPhysicalDevice device);
+    };
 
     void create(Instance * instance);
-    
+  
+protected:
+    bool isSuitable(VkPhysicalDevice device);
+    bool checkDeviceExtensions(VkPhysicalDevice device);
 };
 
 }
--- src/bg2e/render/vulkan/PhysicalDevice.cpp
+++ src/bg2e/render/vulkan/PhysicalDevice.cpp
@@ -6,7 +6,25 @@ namespace bg2e {
 namespace render {
 namespace vulkan {
 
-void PhysicalDevice::create(Instance * instance) {
+PhysicalDevice::QueueFamilyIndices PhysicalDevice::QueueFamilyIndices::get(VkPhysicalDevice device)
+{
+    QueueFamilyIndices result;
+    
+    return result;
+}
+
+void PhysicalDevice::create(Instance * instance)
+{
+}
+
+bool PhysicalDevice::isSuitable(VkPhysicalDevice device)
+{
+    return false;
+}
+
+bool PhysicalDevice::checkDeviceExtensions(VkPhysicalDevice device)
+{
+    return false;
 }
 
 }
