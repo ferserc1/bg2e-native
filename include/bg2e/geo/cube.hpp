510aff620a305b15b8841c20894f50af290ae58d
Fernando Serrano Carpena <ferserc1@gmail.com>
2024-12-03 20:51:31 +0100

    Cube geo functions implemented and Visual Studio project updated

--- include/bg2e/geo/cube.hpp
+++ include/bg2e/geo/cube.hpp
@@ -7,17 +7,17 @@
 namespace bg2e {
 namespace geo {
 
-extern BG2E_API bg2e::geo::MeshP* createCubeP(float width, float height, float depth);
-extern BG2E_API bg2e::geo::MeshPC* createCubePC(float width, float height, float depth);
-extern BG2E_API bg2e::geo::MeshPN* createCubePN(float width, float height, float depth);
-extern BG2E_API bg2e::geo::MeshPU* createCubePU(float width, float height, float depth);
-extern BG2E_API bg2e::geo::MeshPNU* createCubePNU(float width, float height, float depth);
-extern BG2E_API bg2e::geo::MeshPNC* createCubePNC(float width, float height, float depth);
-extern BG2E_API bg2e::geo::MeshPNUC* createCubePNUC(float width, float height, float depth);
-extern BG2E_API bg2e::geo::MeshPNUT* createCubePNUT(float width, float height, float depth);
-extern BG2E_API bg2e::geo::MeshPNUUT* createCubePNUUT(float width, float height, float depth);
+extern BG2E_API bg2e::geo::MeshP* createCubeP(float width, float height, float depth, bool flipFaces = false);
+extern BG2E_API bg2e::geo::MeshPC* createCubePC(float width, float height, float depth, bool flipFaces = false);
+extern BG2E_API bg2e::geo::MeshPN* createCubePN(float width, float height, float depth, bool flipFaces = false);
+extern BG2E_API bg2e::geo::MeshPU* createCubePU(float width, float height, float depth, bool flipFaces = false);
+extern BG2E_API bg2e::geo::MeshPNU* createCubePNU(float width, float height, float depth, bool flipFaces = false);
+extern BG2E_API bg2e::geo::MeshPNC* createCubePNC(float width, float height, float depth, bool flipFaces = false);
+extern BG2E_API bg2e::geo::MeshPNUC* createCubePNUC(float width, float height, float depth, bool flipFaces = false);
+extern BG2E_API bg2e::geo::MeshPNUT* createCubePNUT(float width, float height, float depth, bool flipFaces = false);
+extern BG2E_API bg2e::geo::MeshPNUUT* createCubePNUUT(float width, float height, float depth, bool flipFaces = false);
 
-extern BG2E_API bg2e::geo::MeshPNUUT* createCube(float width, float height, float depth);
+extern BG2E_API bg2e::geo::MeshPNUUT* createCube(float width, float height, float depth, float flipFaces = false);
 
 }
 }
--- src/bg2e/geo/cube.cpp
+++ src/bg2e/geo/cube.cpp
@@ -10,6 +10,7 @@ void createCubeBase(
     float width,
     float height,
     float depth,
+    bool flipFaces,
     std::function<void(VertexPNUT)> vertexCallback,
     std::function<void(uint32_t)> indexCallback,
     std::function<void(uint32_t, uint32_t)> submeshCallback
@@ -175,47 +176,95 @@ void createCubeBase(
         { -1.0f, 0.0f, 0.0f }
     });
 
-    indexCallback(0);
-    indexCallback(1);
-    indexCallback(2);
-    indexCallback(2);
-    indexCallback(3);
-    indexCallback(0);
+    if (flipFaces)
+    {
+        indexCallback(2);
+        indexCallback(1);
+        indexCallback(0);
+        indexCallback(0);
+        indexCallback(3);
+        indexCallback(2);
 
-    indexCallback(4);
-    indexCallback(5);
-    indexCallback(6);
-    indexCallback(6);
-    indexCallback(7);
-    indexCallback(4);
+        indexCallback(6);
+        indexCallback(5);
+        indexCallback(4);
+        indexCallback(4);
+        indexCallback(7);
+        indexCallback(6);
 
-    indexCallback(8);
-    indexCallback(9);
-    indexCallback(10);
-    indexCallback(10);
-    indexCallback(11);
-    indexCallback(8);
+        indexCallback(10);
+        indexCallback(9);
+        indexCallback(8);
+        indexCallback(8);
+        indexCallback(11);
+        indexCallback(10);
 
-    indexCallback(12);
-    indexCallback(13);
-    indexCallback(14);
-    indexCallback(14);
-    indexCallback(15);
-    indexCallback(12);
+        indexCallback(14);
+        indexCallback(13);
+        indexCallback(12);
+        indexCallback(12);
+        indexCallback(15);
+        indexCallback(14);
 
-    indexCallback(16);
-    indexCallback(17);
-    indexCallback(18);
-    indexCallback(18);
-    indexCallback(19);
-    indexCallback(16);
+        indexCallback(18);
+        indexCallback(17);
+        indexCallback(16);
+        indexCallback(16);
+        indexCallback(19);
+        indexCallback(18);
 
-    indexCallback(20);
-    indexCallback(21);
-    indexCallback(22);
-    indexCallback(22);
-    indexCallback(23);
-    indexCallback(20);
+        indexCallback(22);
+        indexCallback(21);
+        indexCallback(20);
+        indexCallback(20);
+        indexCallback(23);
+        indexCallback(22);
+    }
+    else
+    {
+        indexCallback(0);
+        indexCallback(1);
+        indexCallback(2);
+        indexCallback(2);
+        indexCallback(3);
+        indexCallback(0);
+
+        indexCallback(4);
+        indexCallback(5);
+        indexCallback(6);
+        indexCallback(6);
+        indexCallback(7);
+        indexCallback(4);
+
+        indexCallback(8);
+        indexCallback(9);
+        indexCallback(10);
+        indexCallback(10);
+        indexCallback(11);
+        indexCallback(8);
+
+        indexCallback(12);
+        indexCallback(13);
+        indexCallback(14);
+        indexCallback(14);
+        indexCallback(15);
+        indexCallback(12);
+
+        indexCallback(16);
+        indexCallback(17);
+        indexCallback(18);
+        indexCallback(18);
+        indexCallback(19);
+        indexCallback(16);
+
+        indexCallback(20);
+        indexCallback(21);
+        indexCallback(22);
+        indexCallback(22);
+        indexCallback(23);
+        indexCallback(20);
+    }
+
 
     submeshCallback(0, 6);
     submeshCallback(6, 6);
@@ -225,28 +274,79 @@ void createCubeBase(
     submeshCallback(30, 6);
 }
 
-bg2e::geo::MeshP* createCubeP(float width, float height, float depth)
+bg2e::geo::MeshP* createCubeP(float width, float height, float depth, bool flipFaces)
 {
-    return nullptr;
+    auto result = new MeshP();
+    createCubeBase(
+        width,
+        height,
+        depth,
+        flipFaces,
+        [&](VertexPNUT vertex) {
+            result->vertices.push_back({
+                vertex.position
+            });
+        }, [&](uint32_t index) {
+            result->indices.push_back(index);
+        }, [&](uint32_t base, uint32_t count) {
+            result->submeshes.push_back({ base, count });
+        }   
+    );
+    return result;
 }
 
-bg2e::geo::MeshPC* createCubePC(float width, float height, float depth)
+bg2e::geo::MeshPC* createCubePC(float width, float height, float depth, bool flipFaces)
 {
-    return nullptr;
+    auto result = new MeshPC();
+    createCubeBase(
+        width,
+        height,
+        depth,
+        flipFaces,
+        [&](VertexPNUT vertex) {
+            result->vertices.push_back({
+                vertex.position,
+                { 1.0f, 1.0f, 1.0f, 1.0f }
+            });
+        }, [&](uint32_t index) {
+            result->indices.push_back(index);
+        }, [&](uint32_t base, uint32_t count) {
+            result->submeshes.push_back({ base, count });
+        }
+    );
+    return result;
 }
 
-bg2e::geo::MeshPN* createCubePN(float width, float height, float depth)
+bg2e::geo::MeshPN* createCubePN(float width, float height, float depth, bool flipFaces)
 {
-    return nullptr;
+    auto result = new MeshPN();
+    createCubeBase(
+        width,
+        height,
+        depth,
+        flipFaces,
+        [&](VertexPNUT vertex) {
+            result->vertices.push_back({
+                vertex.position,
+                vertex.normal
+            });
+        }, [&](uint32_t index) {
+            result->indices.push_back(index);
+        }, [&](uint32_t base, uint32_t count) {
+            result->submeshes.push_back({ base, count });
+        }
+    );
+    return result;
 }
 
-bg2e::geo::MeshPU* createCubePU(float width, float height, float depth)
+bg2e::geo::MeshPU* createCubePU(float width, float height, float depth, bool flipFaces)
 {
     auto result = new MeshPU();
     createCubeBase(
         width,
         height,
         depth,
+        flipFaces,
         [&](VertexPNUT vertex) {
             result->vertices.push_back({
                 vertex.position,
@@ -261,34 +361,128 @@ bg2e::geo::MeshPU* createCubePU(float width, float height, float depth)
     return result;
 }
 
-bg2e::geo::MeshPNU* createCubePNU(float width, float height, float depth)
+bg2e::geo::MeshPNU* createCubePNU(float width, float height, float depth, bool flipFaces)
 {
-    return nullptr;
+    auto result = new MeshPNU();
+    createCubeBase(
+        width,
+        height,
+        depth,
+        flipFaces,
+        [&](VertexPNUT vertex) {
+            result->vertices.push_back({
+                vertex.position,
+                vertex.normal,
+                vertex.texCoord0
+            });
+        }, [&](uint32_t index) {
+            result->indices.push_back(index);
+        }, [&](uint32_t base, uint32_t count) {
+            result->submeshes.push_back({ base, count });
+        }
+    );
+    return result;
 }
 
-bg2e::geo::MeshPNC* createCubePNC(float width, float height, float depth)
+bg2e::geo::MeshPNC* createCubePNC(float width, float height, float depth, bool flipFaces)
 {
-    return nullptr;
+    auto result = new MeshPNC();
+    createCubeBase(
+        width,
+        height,
+        depth,
+        flipFaces,
+        [&](VertexPNUT vertex) {
+            result->vertices.push_back({
+                vertex.position,
+                vertex.normal,
+                { 1.0f, 1.0f, 1.0f, 1.0f }
+            });
+        }, [&](uint32_t index) {
+            result->indices.push_back(index);
+        }, [&](uint32_t base, uint32_t count) {
+            result->submeshes.push_back({ base, count });
+        }
+    );
+    return result;
 }
 
-bg2e::geo::MeshPNUC* createCubePNUC(float width, float height, float depth)
+bg2e::geo::MeshPNUC* createCubePNUC(float width, float height, float depth, bool flipFaces)
 {
-    return nullptr;
+    auto result = new MeshPNUC();
+    createCubeBase(
+        width,
+        height,
+        depth,
+        flipFaces,
+        [&](VertexPNUT vertex) {
+            result->vertices.push_back({
+                vertex.position,
+                vertex.normal,
+                vertex.texCoord0,
+                { 1.0f, 1.0f, 1.0f, 1.0f }
+            });
+        }, [&](uint32_t index) {
+            result->indices.push_back(index);
+        }, [&](uint32_t base, uint32_t count) {
+            result->submeshes.push_back({ base, count });
+        }
+    );
+    return result;
 }
 
-bg2e::geo::MeshPNUT* createCubePNUT(float width, float height, float depth)
+bg2e::geo::MeshPNUT* createCubePNUT(float width, float height, float depth, bool flipFaces)
 {
-    return nullptr;
+    auto result = new MeshPNUT();
+    createCubeBase(
+        width,
+        height,
+        depth,
+        flipFaces,
+        [&](VertexPNUT vertex) {
+            result->vertices.push_back({
+                vertex.position,
+                vertex.normal,
+                vertex.texCoord0,
+                vertex.tangent
+            });
+        }, [&](uint32_t index) {
+            result->indices.push_back(index);
+        }, [&](uint32_t base, uint32_t count) {
+            result->submeshes.push_back({ base, count });
+        }
+    );
+    return result;
 }
 
-bg2e::geo::MeshPNUUT* createCubePNUUT(float width, float height, float depth)
+bg2e::geo::MeshPNUUT* createCubePNUUT(float width, float height, float depth, bool flipFaces)
 {
-    return nullptr;
+    auto result = new MeshPNUUT();
+    createCubeBase(
+        width,
+        height,
+        depth,
+        flipFaces,
+        [&](VertexPNUT vertex) {
+            result->vertices.push_back({
+                vertex.position,
+                vertex.normal,
+                vertex.texCoord0,
+                vertex.texCoord0,
+				vertex.tangent
+            });
+        }, [&](uint32_t index) {
+            result->indices.push_back(index);
+        }, [&](uint32_t base, uint32_t count) {
+            result->submeshes.push_back({ base, count });
+        }
+    );
+    return result;
 }
 
-bg2e::geo::MeshPNUUT* createCube(float width, float height, float depth)
+bg2e::geo::MeshPNUUT* createCube(float width, float height, float depth, bool flipFaces)
 {
-    return nullptr;
+    return createCubePNUUT(width, height, depth, flipFaces);
 }
 
 }
