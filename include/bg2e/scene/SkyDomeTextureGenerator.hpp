96ad15803d93d159538ac9e2d556a2ea1c185e0e
Fernando Serrano Carpena <ferserc1@gmail.com>
2025-05-31 12:01:12 +0200

    SkyDomeTextureGenerator added

---
+++ include/bg2e/scene/SkyDomeTextureGenerator.hpp
@@ -0,0 +1,17 @@
+//
+//  ProceduralSkyDomeGenerator.hpp
+
+#pragma once
+
+#include <bg2e/base/Texture.hpp>
+
+namespace bg2e {
+namespace scene {
+
+class BG2E_API SkyDomeTextureGenerator : public base::ProceduralTextureGenerator {
+public:
+    
+};
+
+}
+}
