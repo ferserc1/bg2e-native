8fc9e5fa1b61dd71115e1b0d8be8038ae4e9b7a4
Fernando Serrano Carpena <ferserc1@gmail.com>
2025-11-27 14:54:59 +0100

    FindVulkan.cmake implemented in Windows

--- cmake/FindVulkan.cmake
+++ cmake/FindVulkan.cmake
@@ -2,11 +2,9 @@
 # Exports:
 # - VULKAN_INCLUDE_PATH Include directory
 # - VULKAN_LIB          Link library (.lib/.so/.dylib)
-# - VULKAN_BIN          Binary library (.dll/.so/.dylib)
 # - VULKAN_LIB_PATH     libraries path
-# - VULKAN_BIN_PATH     Binary libraries path
-# - VULKAN_BIN_NAME     Binary library name (example: libvulkan.1.4.313.dylib)
-# - VULKAN_BIN_VERSION  Binary library version (example: 1.4.313)
+# - VULKAN_LIB_NAME     Binary library name (example: libvulkan.1.4.313.dylib)
+# - VULKAN_LIB_VERSION  Binary library version (example: 1.4.313)
 
 find_path(VULKAN_INCLUDE_PATH vulkan.h
     PATHS
@@ -22,24 +20,23 @@ if(APPLE)
         "${VULKAN_LIB_PATH}/libvulkan.*.dylib"
     )
     list(GET VULKAN_LIB_FILES 0 VULKAN_LIB)
-    set(VULKAN_BIN ${VULKAN_LIB})
-    set(VULKAN_BIN_PATH ${VULKAN_LIB_PATH})
 
-    get_filename_component(VULKAN_BIN_NAME "${VULKAN_BIN}" NAME)
+    get_filename_component(VULKAN_LIB_NAME "${VULKAN_LIB}" NAME)
 
     string(REGEX REPLACE "^libvulkan\\.([0-9\\.]+)\\.dylib$" "\\1"
-        VULKAN_BIN_VERSION "${VULKAN_BIN_NAME}"
+        VULKAN_LIB_VERSION "${VULKAN_LIB_NAME}"
     )
 elseif(APPLE AND NOT UNIX)
     # TODO: implement this
-elseif(WINDOWS)
-    # TODO: implement this
+else()
+    set(VULKAN_LIB_PATH "${VULKAN_SDK}/Lib")
+    set(VULKAN_LIB_NAME "vulkan-1.lib")
+    set(VULKAN_LIB "${VULKAN_LIB_PATH}/${VULKAN_LIB_NAME}")
+    set(VULKAN_LIB_VERSION "1")
 endif()
 
 message(STATUS "VULKAN_INCLUDE_PATH: ${VULKAN_INCLUDE_PATH}")
 message(STATUS "VULKAN_LIB: ${VULKAN_LIB}")
-message(STATUS "VULKAN_BIN: ${VULKAN_BIN}")
 message(STATUS "VULKAN_LIB_PATH: ${VULKAN_LIB_PATH}")
-message(STATUS "VULKAN_BIN_PATH ${VULKAN_BIN_PATH}")
-message(STATUS "VULKAN_BIN_NAME: ${VULKAN_BIN_NAME}")
-message(STATUS "VULKAN_BIN_VERSION: ${VULKAN_BIN_VERSION}")
+message(STATUS "VULKAN_LIB_NAME: ${VULKAN_LIB_NAME}")
+message(STATUS "VULKAN_LIB_VERSION: ${VULKAN_LIB_VERSION}")
