2417b9f5f14c7e000aa1ee39616100280938ad48
Fernando Serrano Carpena <ferserc1@gmail.com>
2025-12-05 13:12:50 +0100

    nativefiledialog submodule removed and added as files

--- lib/cmake/deps.cmake
+++ lib/cmake/deps.cmake
@@ -17,20 +17,20 @@ set(BG2SCENE_INCLUDE "${BG2IO_PATH}/src/bg2-scene")
 file(GLOB IMGUI_SRC "${THIRD_PARTY_PATH}/imgui/*.cpp")
 set(IMGUI_INCLUDE "${THIRD_PARTY_PATH}/imgui")
 
-# nativefiledialog-extended
+# nativefiledialog
 if(APPLE)
-    set(NFD_SRC "${THIRD_PARTY_PATH}/nativefiledialog-extended/src/nfd_cocoa.m")
+    set(NFD_SRC "${THIRD_PARTY_PATH}/nativefiledialog/src/nfd_cocoa.m")
     enable_language(OBJC)
     enable_language(OBJCXX)
     set_source_files_properties("${NFD_SRC}"
         PROPERTIES LANGUAGE OBJC
     )
 elseif(UNIX AND NOT APPLE)
-    set(NFD_SRC "${THIRD_PARTY_PATH}/nativefiledialog-extended/src/nfd_gtk.cpp")
+    set(NFD_SRC "${THIRD_PARTY_PATH}/nativefiledialog/src/nfd_gtk.cpp")
 else()
-    set(NFD_SRC "${THIRD_PARTY_PATH}/nativefiledialog-extended/src/nfd_win.cpp")
+    set(NFD_SRC "${THIRD_PARTY_PATH}/nativefiledialog/src/nfd_win.cpp")
 endif()
-set(NFD_INCLUDE "${THIRD_PARTY_PATH}/nativefiledialog-extended/src/include")
+set(NFD_INCLUDE "${THIRD_PARTY_PATH}/nativefiledialog/src/include")
 
 # stb_image
 set(STBIMAGE_INCLUDE "${THIRD_PARTY_PATH}/stb_image")
@@ -38,18 +38,8 @@ set(STBIMAGE_INCLUDE "${THIRD_PARTY_PATH}/stb_image")
 # tinyobj
 set(TINYOBJ_INCLUDE "${THIRD_PARTY_PATH}/tinyobj")
 
-# fastgltf has become an extremely complex, verbose library that changes between versions, which is why it has been abandoned in favor of cgltf.
-# simdjson
-# set(SIMDJSON_SRC "${THIRD_PARTY_PATH}/simdjson/simdjson.cpp")
-#set(SIMDJSON_INCLUDE "${THIRD_PARTY_PATH}/simdjson")
-# fastgltf
-# file(GLOB FASTGLTF_SRC "${THIRD_PARTY_PATH}/fastgltf/src/*.cpp")
-# set(FASTGLTF_INCLUDE "${THIRD_PARTY_PATH}/fastgltf/include")
-
 # cgltf
 set(CGLTF_INCLUDE "${THIRD_PARTY_PATH}/cgltf")
-
-message("FastGLTF" "Includes: ${FASTGLTF_INCLUDE}")
 
 # Export final variables
 set(BG2E_THIRD_PARTY_INCLUDE_PATH
