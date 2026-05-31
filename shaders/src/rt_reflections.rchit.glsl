d34ac165ed49ad1f1816022156471e576ed6fbcd
Fernando Serrano Carpena <ferserc1@gmail.com>
2026-05-31 21:16:28 +0200

    closest hit reflection shader updated to debug reflections

--- shaders/src/rt_reflections.rchit.glsl
+++ shaders/src/rt_reflections.rchit.glsl
@@ -1,6 +1,6 @@
 #version 460
 #extension GL_ARB_shading_language_include : require
-#extension GL_EXT_ray_tracing : enable
+#extension GL_EXT_ray_tracing : require
 
 hitAttributeEXT vec2 attribs;
 
@@ -14,7 +14,17 @@ void main() {
     payload.didHit = 1u;
     payload.hitDistance = gl_HitTEXT;
 
-    // TODO: generate the correct hit color for the payload
-    float attenuation = 1.0 - clamp(gl_HitTEXT / 50.0, 0.0, 1.0);
-    payload.hitColor = vec3(0.5) * attenuation;
+    vec3 bary = vec3(
+        1.0 - attribs.x - attribs.y,
+        attribs.x,
+        attribs.y
+    );
+
+    float edge = min(min(bary.x, bary.y), bary.z);
+
+    float line = smoothstep(0.0, 0.02, edge);
+
+    payload.hitColor = mix(vec3(1.0), bary, line);
+    payload.hitDistance = gl_HitTEXT;
+    payload.didHit = 1;
 }
